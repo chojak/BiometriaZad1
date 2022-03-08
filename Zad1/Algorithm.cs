0d419c1dc37dfea90008266dd6d6fee3b7e5c9e9
chojak <chojecki2000@gmail.com>
2022-03-09 00:08:53 +0100

    Add project files.

---
+++ Zad1/Algorithm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace Zad1
+{
+    public static class Algorithm
+    {
+        public static Bitmap HistogramBitmap(Bitmap bitmap)
+        {
+            var data = bitmap.LockBits(
+               new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+               System.Drawing.Imaging.ImageLockMode.ReadWrite,
+               System.Drawing.Imaging.PixelFormat.Format24bppRgb
+           );
+
+            return bitmap;
+        }
+        public static Bitmap Histogram(Bitmap bmp_original)
+        {
+            Bitmap bmp = new Bitmap(bmp_original);
+            var data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb
+            );
+
+            var bmpData = new byte[data.Width * 3 * data.Height];
+            Marshal.Copy(data.Scan0, bmpData, 0, bmpData.Length);
+            // Przerzuci z Bitmapy do tablicy
+
+            int[] histogram = new int[256];
+
+            foreach (byte i in bmpData)
+                ++histogram[i];
+
+            double max = histogram.Max();
+
+            for (int i = 0; i < histogram.Length; i++)
+                histogram[i] = (int)(histogram[i] / max * 512.0);
+
+
+
+            bmpData = new byte[bmpData.Length];
+
+            for (int i = 0; i < bmpData.Length; i++)
+                bmpData[i] = 255;
+
+            MessageBox.Show("Width: " + data.Width +
+    "\nStride: " + data.Stride +
+    "\nHeight: " + data.Height +
+    "Width * 3: " + data.Width * 3);
+
+
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                for (int j = 0; j < histogram[i]; j++)
+                {
+                    int index = i * 3 + (511 - j) * data.Width * 3;
+
+                    //bmpData[index + 0] = 0;
+                    //bmpData[index + 1] = 0;
+                    //bmpData[index + 2] = 0;
+                    // bmpData[index] = 0;
+
+                }
+            }
+
+            for (int i = 0; i < bmpData.Length / 3; i++)
+            {
+                if(i > 69 && i < 2137)
+                     bmpData[i * 3] = 50;
+            } 
+
+            Marshal.Copy(bmpData, 0, data.Scan0, bmpData.Length);
+            // Przerzuci z tablicy do Bitmapy
+
+            bmp.UnlockBits(data);
+            return bmp;
+        }
+
+        public static Bitmap BinaryThreshold(Bitmap bmp, byte threshold)
+        {
+            var data = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb
+            );
+
+            /*
+             * 3x4 => 12 pikseli
+             * 9x4 => 36 bajtów (RGB)
+             */
+
+            // Width - szerokość w pikselach
+            // Stride - szerokość w bajtach
+            // Stride => Width * 3
+            var bmpData = new byte[data.Width * 3 * data.Height];
+
+            Marshal.Copy(data.Scan0, bmpData, 0, bmpData.Length);
+            // Przerzuci z Bitmapy do tablicy
+
+
+            for (int i = 0; i < bmpData.Length; i += 3)
+            {
+                byte r = bmpData[i + 0];
+                byte g = bmpData[i + 1];
+                byte b = bmpData[i + 2];
+
+                byte mean = (byte)((r + g + b) / 3);
+
+                bmpData[i + 0] =
+                bmpData[i + 1] =
+                bmpData[i + 2] = mean > threshold ? byte.MaxValue : byte.MinValue;
+            }
+
+            Marshal.Copy(bmpData, 0, data.Scan0, bmpData.Length);
+
+            bmp.UnlockBits(data);
+            return bmp;
+        }
+    }
+}
---
+++ Zad1/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Source = "";
+        public Bitmap ImageBitmap;
+        public Bitmap HistogramBitmap;
+
+        // https://stackoverflow.com/questions/26260654/wpf-converting-bitmap-to-imagesource
+        private BitmapImage BitmapToImageSource(System.Drawing.Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+                return bitmapimage;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Image.MaxHeight = 500;
+            Image.MaxWidth = 800;
+
+            //Histogram.MaxHeight = 500;
+            //Histogram.MaxWidth = 800;
+
+
+            ThresholdLevelTextBox.Text = "Threshold level: 0";
+        }
+
+        private void SwapButton_Click(object sender, RoutedEventArgs e)
+        {
+            Image.Visibility = Image.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+            Histogram.Visibility = Histogram.Visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+
+            SwapButton.Content = SwapButton.Content == "BinaryThreshold" ? "Histogram" : "BinaryThreshold";
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (Source != "")
+            {
+                ImageBitmap = Algorithm.BinaryThreshold(new Bitmap(Source), (byte)Slider.Value);
+                Image.Source = BitmapToImageSource(ImageBitmap);
+                ThresholdLevelTextBox.Text = "Threshold level: " + Math.Round(Slider.Value, 0);
+            }
+        }
+
+        private void ChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Source = openFileDialog.FileName;
+                ImageBitmap = new Bitmap(Source);
+                Image.Source = BitmapToImageSource(ImageBitmap);
+                HistogramBitmap = Algorithm.Histogram(new Bitmap(Source));
+                Histogram.Source = BitmapToImageSource(HistogramBitmap);
+            }
+
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // czemu == nie dziala?!?!?
+            if (SwapButton.Content.Equals("Histogram"))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+                saveFileDialog.Title = "Save an Image File";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    ImageBitmap.Save(saveFileDialog.FileName);
+                }
+            }
+            else
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+                saveFileDialog.Title = "Save a histogram";
+                saveFileDialog.ShowDialog();
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    HistogramBitmap.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+    }
+}
